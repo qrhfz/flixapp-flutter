df25eba3e3a4214c25aab4539566b26516f80d6d
q <mboh.dong@gmail.com>
2022-01-23 13:13:47 +0700

    tv equatable

---
+++ lib/domain/entities/episode.dart
@@ -0,0 +1,13 @@
+import 'dart:ffi';
+
+class Episode {
+  int id;
+  String? title;
+  String? overview;
+  String? posterPath;
+  int? seasonNumber;
+  DateTime? airDate;
+  Double? voteAverage;
+  int? voteCount;
+  String? stillPath;
+}
--- lib/domain/entities/tv.dart
+++ lib/domain/entities/tv.dart
@@ -1,19 +1,62 @@
-class Tv {
-  bool? adult;
-  String? backdropPath;
-  DateTime? firstAirDate;
-  DateTime? lastAirDate;
-  List<int>? genreIds;
-  int id;
-  int? numberOfEpisodes;
-  int? numberOfSeasons;
-  String? originalTitle;
-  String? overview;
-  double? popularity;
-  String? posterPath;
-  String? releaseDate;
-  String? title;
-  bool? video;
-  double? voteAverage;
-  int? voteCount;
+import 'package:equatable/equatable.dart';
+
+class Tv extends Equatable {
+  final bool? adult;
+  final String? backdropPath;
+  final DateTime? firstAirDate;
+  final DateTime? lastAirDate;
+  final List<int>? genreIds;
+  final int id;
+  final int? numberOfEpisodes;
+  final int? numberOfSeasons;
+  final String? originalTitle;
+  final String? overview;
+  final double? popularity;
+  final String? posterPath;
+  final String? releaseDate;
+  final String? title;
+  final bool? video;
+  final double? voteAverage;
+  final int? voteCount;
+
+  Tv({
+    required this.id,
+    required this.adult,
+    required this.backdropPath,
+    required this.firstAirDate,
+    required this.genreIds,
+    required this.lastAirDate,
+    required this.numberOfEpisodes,
+    required this.numberOfSeasons,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        adult,
+        backdropPath,
+        firstAirDate,
+        genreIds,
+        lastAirDate,
+        numberOfEpisodes,
+        numberOfSeasons,
+        originalTitle,
+        overview,
+        popularity,
+        posterPath,
+        releaseDate,
+        title,
+        video,
+        voteAverage,
+        voteCount,
+      ];
 }
