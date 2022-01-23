0a6a4f77bc5721b6d7bfbf8b5ad7cb1e7a207c5e
q <mboh.dong@gmail.com>
2022-01-23 16:37:17 +0700

    tv model fromjson tojson

--- lib/data/models/tv_model.dart
+++ lib/data/models/tv_model.dart
@@ -1,38 +1,46 @@
-import 'package:ditonton/domain/entities/tv.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-class TvModel extends Tv {
+part 'tv_model.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake)
+class TvModel {
   TvModel({
-    required int id,
-    required bool adult,
-    required String backdropPath,
-    required DateTime firstAirDate,
-    required List<int> genreIds,
-    required DateTime lastAirDate,
-    required int numberOfEpisodes,
-    required int numberOfSeasons,
-    required String originalTitle,
-    required String overview,
-    required double popularity,
-    required String posterPath,
-    required DateTime releaseDate,
-    required String title,
-    required double voteAverage,
-    required int voteCount,
-  }) : super(
-          id: id,
-          adult: adult,
-          backdropPath: backdropPath,
-          firstAirDate: firstAirDate,
-          posterPath: posterPath,
-          title: title,
-          lastAirDate: lastAirDate,
-          numberOfSeasons: numberOfSeasons,
-          numberOfEpisodes: numberOfEpisodes,
-          voteCount: voteCount,
-          voteAverage: voteAverage,
-          genreIds: genreIds,
-          popularity: popularity,
-          originalTitle: originalTitle,
-          overview: overview,
-        );
+    required this.id,
+    required this.adult,
+    required this.backdropPath,
+    required this.firstAirDate,
+    required this.genreIds,
+    required this.lastAirDate,
+    required this.numberOfEpisodes,
+    required this.numberOfSeasons,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.title,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  final bool? adult;
+  final String? backdropPath;
+  final DateTime? firstAirDate;
+  final DateTime? lastAirDate;
+  final List<int>? genreIds;
+  final int id;
+  final int? numberOfEpisodes;
+  final int? numberOfSeasons;
+  final String? originalTitle;
+  final String? overview;
+  final double? popularity;
+  final String? posterPath;
+  @JsonKey(name: 'name')
+  final String? title;
+  final double? voteAverage;
+  final int? voteCount;
+
+  factory TvModel.fromJson(Map<String, dynamic> json) =>
+      _$TvModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$TvModelToJson(this);
 }
