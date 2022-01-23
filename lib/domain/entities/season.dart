5c75bedfcde528df03adc76ed92046013090df23
q <mboh.dong@gmail.com>
2022-01-23 12:34:39 +0700

    membuat entity season

---
+++ lib/domain/entities/season.dart
@@ -0,0 +1,8 @@
+class Season {
+  int id;
+  String? title;
+  String? overview;
+  String? posterPath;
+  int? seasonNumber;
+  DateTime? airDate;
+}
---
+++ lib/domain/entities/tv.dart
@@ -0,0 +1,19 @@
+class Tv {
+  bool? adult;
+  String? backdropPath;
+  DateTime? firstAirDate;
+  DateTime? lastAirDate;
+  List<int>? genreIds;
+  int id;
+  int? numberOfEpisodes;
+  int? numberOfSeasons;
+  String? originalTitle;
+  String? overview;
+  double? popularity;
+  String? posterPath;
+  String? releaseDate;
+  String? title;
+  bool? video;
+  double? voteAverage;
+  int? voteCount;
+}
