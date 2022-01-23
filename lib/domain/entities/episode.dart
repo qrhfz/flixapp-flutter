ded6df2a11bcb0d8b7f3cac897442eab0b24dc51
q <mboh.dong@gmail.com>
2022-01-23 13:35:42 +0700

    Update episode.dart

--- lib/domain/entities/episode.dart
+++ lib/domain/entities/episode.dart
@@ -1,13 +1,23 @@
 import 'dart:ffi';
 
 class Episode {
-  int id;
-  String? title;
-  String? overview;
-  String? posterPath;
-  int? seasonNumber;
-  DateTime? airDate;
-  Double? voteAverage;
-  int? voteCount;
-  String? stillPath;
+  final int id;
+  final String title;
+  final String overview;
+  final String? stillPath;
+  final int seasonNumber;
+  final DateTime airDate;
+  final Double voteAverage;
+  final int voteCount;
+
+  Episode({
+    required this.id,
+    required this.title,
+    required this.overview,
+    required this.stillPath,
+    required this.seasonNumber,
+    required this.airDate,
+    required this.voteAverage,
+    required this.voteCount,
+  });
 }
