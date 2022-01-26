cb504a1bd27a7ea4e25886c451833c9bde9a43f4
q <mboh.dong@gmail.com>
2022-01-26 13:36:48 +0700

    get top rated tv

---
+++ test/domain/usecases/get_top_rated_tv_series_test.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/domain/repositories/tv_repository.dart';
+import 'package:ditonton/domain/usecases/get_top_rated_tv_series.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late final TvRepository tvRepository;
+  late final GetTopRatedTvSeries usecase;
+  final List<Tv> dummyTv = [];
+  setUp(() {
+    tvRepository = MockTvRepository();
+    usecase = GetTopRatedTvSeries(tvRepository);
+  });
+  group('Get top rated tv series', () {
+    test('execute', () async {
+      when(tvRepository.getTopRatedTvSeries())
+          .thenAnswer((_) async => Right(dummyTv));
+      final tvSeries = await usecase.execute();
+      assert(tvSeries is Right<Failure, List<Tv>>);
+    });
+  });
+}
