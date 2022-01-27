dde206a0e500a72d7a213ae8733ce45eb6dd2207
Qori El-Hafizh <mboh.dong@gmail.com>
2022-01-27 18:10:28 +0700

    remove unused import

--- lib/domain/usecases/tvshow/get_airing_tv_shows.dart
+++ lib/domain/usecases/tvshow/get_airing_tv_shows.dart
@@ -1,7 +1,4 @@
-import 'package:dartz/dartz.dart';
-import 'package:ditonton/common/failure.dart';
 import 'package:ditonton/common/typealias.dart';
-import 'package:ditonton/domain/entities/tv_show.dart';
 import 'package:ditonton/domain/repositories/tv_show_repository.dart';
 import 'package:ditonton/common/usecase.dart';
 
