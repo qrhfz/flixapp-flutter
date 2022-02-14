106b4c21919585648a6bf75614f123ca1c0a313c
Qori El-Hafizh <contact@qori.dev>
2022-02-14 20:03:17 +0700

    tv front

--- lib/presentation/pages/tv_front_page.dart
+++ lib/presentation/pages/tv_front_page.dart
@@ -1,3 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ditonton/common/constants.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
 import 'package:flutter/material.dart';
 
 class TvFrontPage extends StatelessWidget {
@@ -12,6 +15,48 @@ class TvFrontPage extends StatelessWidget {
         title: Text('TV SERIES'),
       ),
       body: Container(),
+    );
+  }
+}
+
+class _HorizontalTvListView extends StatelessWidget {
+  final List<TvShow> tvlist;
+
+  _HorizontalTvListView(this.tvlist);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final tv = tvlist[index];
+          return Container(
+            padding: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () {
+                // Navigator.pushNamed(
+                //   context,
+                //   MovieDetailPage.ROUTE_NAME,
+                //   arguments: movie.id,
+                // );
+              },
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(16)),
+                child: CachedNetworkImage(
+                  imageUrl: '$BASE_IMAGE_URL${tv.posterPath}',
+                  placeholder: (context, url) => Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+              ),
+            ),
+          );
+        },
+        itemCount: tvlist.length,
+      ),
     );
   }
 }
